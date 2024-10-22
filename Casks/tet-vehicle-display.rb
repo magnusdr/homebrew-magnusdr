22b5dd714c50a8aff998afd489feb8db919123f9
magnusdr <magnusdr@users.noreply.github.com>
2024-10-22 11:12:20 +0000

    Commit from GitHub Actions (🚀 Update casks)

--- Casks/tet-vehicle-display.rb
+++ Casks/tet-vehicle-display.rb
@@ -1,7 +1,7 @@
 cask "tet-vehicle-display" do
-    version "0.8.0"
-    sha256 "2e41f82ac17e677b08d32308cc43cee3b01bfc04f7f326b74d2efb4d784a79f0"
-    url "https://vehicle-display.tet.digital/assets/tet-vehicle-display_macos_v0.8.0.tar.gz"
+    version "0.8.4"
+    sha256 "9cb807f6805aa1db9cce77556be43458eb2a0d9cbd3bf5dca73aaa1c055dcace"
+    url "https://vehicle-display.tet.digital/assets/tet-vehicle-display_macos_v0.8.4.tar.gz"
     name "TET Vehicle Display"
     desc "Travel information on board the vehicle"
     homepage "https://vehicle-display.tet.digital"
@@ -11,6 +11,6 @@ cask "tet-vehicle-display" do
         regex(/(\d+(?:\.\d+)+)/i)
     end
 
-    binary 'tet-vehicle-display_macos_v0.8.0/vdloader'
-    binary 'tet-vehicle-display_macos_v0.8.0/findmyvehicle'
+    binary 'tet-vehicle-display_macos_v0.8.4/vdloader'
+    binary 'tet-vehicle-display_macos_v0.8.4/findmyvehicle'
 end
